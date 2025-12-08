71f2c17556605dcaf6edee69ccbcacba2502b0e3
unknown <musadiq2ali@gmail.com>
2025-12-08 19:38:40 +0500

    intial commit

---
+++ basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234
+let accountEmail = "musadiq2ali@gmail.com"
+var accountPassword = "123456"
+accountCity = "Lahore"
+let accountState;
+
+accountEmail = "musa@gmail.com"
+accountPassword ="12121"
+accountCity = "Isl"
+// accountId = 4
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// console.log(accountId);
