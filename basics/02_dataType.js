ddf71d1afdb9c03b7490c9fb0f26879d9f1edcf4
unknown <musadiq2ali@gmail.com>
2025-12-16 19:35:41 +0500

    intial commit

--- basics/02_dataType.js
+++ basics/02_dataType.js
@@ -1,3 +1,18 @@
 "use strict"; //  treat all JS code as newer version code
+let age = 5
 
 // alert(3+3) // we are using node.js not browser thats why alert not working
+// datatypes with examples
+// number => 2 to power 53 = (range of number datatype)
+// bigint (for more number than number datatype)
+// string => ""
+// null =>  means standalone value
+// undefine => means no value assing to variable
+// symbol => for uniquess 
+
+// -----------------------------------------------------------------------------------------------------------
+//  Object 
+console.log(typeof "ali")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
