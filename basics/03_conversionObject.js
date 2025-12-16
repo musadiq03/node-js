2bf76bb3b1a907c9a169d53c7a496297dc66dd2b
unknown <musadiq2ali@gmail.com>
2025-12-17 00:48:50 +0500

    intial commit

--- basics/03_conversionObject.js
+++ basics/03_conversionObject.js
@@ -1,1 +1,35 @@
-console.log("hii")
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "ali"
+console.log(typeof(score))
+console.log(score)
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber)
+ console.log(valueInNumber)
+
+// Investigation Study
+
+//  "33" => 33 (number)
+//  "33abc" => NaN (not a number but type is convert to number
+//  true =>  1; false => 0 
+// --------------------------------------------------------------------------------------------------------------
+// let  isLoggedIn = 1
+// let  isLoggedIn = ""
+let  isLoggedIn = "Ali"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// Investigation Study
+
+// 1 => true; 0 => false
+// "" => false; "Ali" => true
+
+// ----------------------------------------------------------------------------------------------
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
