b84e8a1c2ae02536e4083f3d279fa6747256aa0d
unknown <musadiq2ali@gmail.com>
2025-12-20 19:34:28 +0500

    intial commit

---
+++ basics/04_comparision.js
@@ -0,0 +1,19 @@
+// Type script in java sript 
+
+console.log("2" > 1)
+console.log("02" > 1)
+console.log("************comparision with null******************")
+
+console.log(null > 0)
+console.log(null < 0)
+console.log(null >= 0)
+console.log(null == 0)
+console.log("***********comparision with undefined*******************")
+
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined == 0)
+
+console.log("***********strictly comparision (in this data type also checked) === *******************")
+
+console.log("2" === 2)
