07a61d940e089ffaf1272c6fa56dabad806039be
Ali <musadiq2ali@gmail.com>
2025-12-29 15:18:12 +0500

    intial commit

--- basics/06_nums_and_math.js
+++ basics/06_nums_and_math.js
@@ -42,4 +42,6 @@ console.log(Math.floor(Math.random()*10) + 1)
 const min = 10
 const  max = 20
 
+// this line generate output random numbers range 10-20 
 console.log(Math.floor(Math.random()* (max - min + 1))+ min)
+
